9bf0f481a1ae0a0fb4d206160b57f415f6d4b264
ryuma-murakami <241606278+ryuma-murakami@users.noreply.github.com>
2025-12-29 23:20:41 +0900

    feat: add layout component

---
+++ app/foo/layout.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from 'react';
+
+type RootLayoutProps = {
+  children: ReactNode;
+};
+
+const Layout = ({ children }: RootLayoutProps) => {
+  return (
+    <>
+      <nav>
+        <a href="/foo">foo</a>/<a href="/foo/bar">bar</a>
+      </nav>
+      {children}
+    </>
+  );
+};
+
+export default Layout;
