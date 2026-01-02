b1aaaab23128f7580f9759056f29aea99adc60e3
ryuma-murakami <241606278+ryuma-murakami@users.noreply.github.com>
2026-01-02 16:54:39 +0900

    feat: add fetch api

--- app/item/[id]/page.tsx
+++ app/item/[id]/page.tsx
@@ -10,14 +10,23 @@ export const generateMetadata = async ({
   params,
 }: PageProps): Promise<Metadata> => {
   const { id } = await params;
+  const response = await fetch(`http://localhost:3000/api?id=${id}`);
+  const item = await response.json();
   return {
-    title: `Item${id}`,
+    title: item.name,
   };
 };
 
 const Item = async ({ params }: PageProps) => {
   const { id } = await params;
-  return <h1>Item{id}ページ</h1>;
+  const response = await fetch(`http://localhost:3000/api?id=${id}`);
+  const item = await response.json();
+  return (
+    <div>
+      <h2>{item.name}</h2>
+      <p>価格: {item.price}円</p>
+    </div>
+  );
 };
 
 export default Item;
