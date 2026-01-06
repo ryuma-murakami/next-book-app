7692fbfedf1053409a784276a26d7342218ec6e4
ryuma-murakami <241606278+ryuma-murakami@users.noreply.github.com>
2026-01-06 13:04:11 +0900

    feat: add form with validation

--- app/server-functions/actions.ts
+++ app/server-functions/actions.ts
@@ -1,9 +1,23 @@
 'use server';
 
-let count = 0;
+export type ActionState = {
+  error?: string;
+  success?: string;
+};
 
-export const incrementCount = async () => {
-  await new Promise(resolve => setTimeout(resolve, 2000));
-  count++;
-  return count;
+export const checkText = async (prevState: ActionState, formData: FormData) => {
+  const text = formData.get('myText') as string;
+  if (!text) {
+    return {
+      error: '必須項目です。テキストを入力してください',
+    };
+  }
+  if (text.length > 10) {
+    return {
+      error: 'テキストは10文字以下で入力してください',
+    };
+  }
+  return {
+    success: `入力されたテキスト「${text}」は正しいです`,
+  };
 };
