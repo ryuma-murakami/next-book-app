a6de6e1041bcd064eea06b4fde606a9d31ce55c0
ryuma-murakami <241606278+ryuma-murakami@users.noreply.github.com>
2026-01-10 12:47:25 +0900

    refactor: improve form and validation logic

--- app/server-functions/actions.ts
+++ app/server-functions/actions.ts
@@ -1,23 +1,30 @@
 'use server';
 
-export type ActionState = {
-  error?: string;
-  success?: string;
-};
+export type UpdateFormResult =
+  | {
+      result: string;
+      errors: null;
+    }
+  | {
+      result?: never;
+      errors: string[];
+    };
 
-export const checkText = async (prevState: ActionState, formData: FormData) => {
-  const text = formData.get('myText') as string;
-  if (!text) {
-    return {
-      error: '必須項目です。テキストを入力してください',
-    };
-  }
-  if (text.length > 10) {
-    return {
-      error: 'テキストは10文字以下で入力してください',
-    };
-  }
-  return {
-    success: `入力されたテキスト「${text}」は正しいです`,
-  };
-};
+export async function updateForm(title: string): Promise<UpdateFormResult> {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      const errors: string[] = [];
+      if (title === '') {
+        errors.push('必須項目です。テキストを入力してください');
+      }
+      if (title.length > 10) {
+        errors.push('テキストは10文字以下で入力してください');
+      }
+      if (errors.length > 0) {
+        resolve({ errors });
+      } else {
+        resolve({ result: title, errors: null });
+      }
+    }, 1000);
+  });
+}
